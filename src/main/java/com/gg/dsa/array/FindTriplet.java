b576722b7ecb79d158abf960dffd798e0c87391a
gopalghosh <gopal.ghosh89@gmail.com>
2021-09-30 21:54:12 +0530

    Find triplet

---
+++ src/main/java/com/gg/dsa/array/FindTriplet.java
@@ -0,0 +1,34 @@
+package com.gg.dsa.array;
+
+public class FindTriplet {
+
+
+    public int findTriplet(int arr[]) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum[] = new int[4];
+            int k = 0;
+            for (int j = i + 1; j < arr.length; j++) {
+                sum[k] = arr[i] + arr[j];
+            }
+
+            for (int j = 0; j < arr.length; j++) {
+                for (int l = 0; l < sum.length; l++) {
+                    if (arr[j] == sum[l]) {
+                        System.out.println(arr[i] + ">>>" + (sum[l] - arr[i]));
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println("count " + count);
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 5, 3, 2};
+        FindTriplet f = new FindTriplet();
+        f.findTriplet(arr);
+
+    }
+}
--- src/main/java/com/gg/dsa/stack/LongestValidParenthesis.java
+++ src/main/java/com/gg/dsa/stack/LongestValidParenthesis.java
@@ -1,4 +1,38 @@
 package com.gg.dsa.stack;
 
+import java.util.Stack;
+
 public class LongestValidParenthesis {
+    public static void main(String[] args) {
+        String input = "()(()))))))";
+        System.out.println(maxValidLength(input));
+    }
+
+
+    public static int maxValidLength(String input) {
+        int result = 0;
+        Stack<Integer> s = new Stack<>();
+        s.push(-1);
+        int count = 0;
+        while (count < input.length()) {
+            char ch = input.charAt(count);
+            if (ch == '(') {
+                s.push(count);
+            } else {
+                if (!s.isEmpty()) {
+                    s.pop();
+                }
+                if (!s.isEmpty()) {
+                    result = Math.max(result,
+                            count - s.peek());
+                }
+                else {
+                    s.push(count);
+                }
+            }
+            count++;
+        }
+        return result;
+    }
+
 }
